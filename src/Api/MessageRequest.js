75f648e731ee6b4f011ee723f4bdccce79f78b44
Abhinav A <abhinav.kky@gmail.com>
2022-11-09 14:08:51 +0530

    added base url to .env

--- src/Api/MessageRequest.js
+++ src/Api/MessageRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API = axios.create({ baseURL: "http://localhost:5000" })
+const API = axios.create({ baseURL: `${ process.env.REACT_APP_BACKEND_URL }` });
 
 export const getMessages = (id) => API.get(`/messages/${id}`)
 export const addMessage = (data) => API.post('/messages/', data)
